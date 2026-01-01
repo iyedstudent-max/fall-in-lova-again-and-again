b9b1b1ffcb59f0646e1eba216dd4b9116d08e300
iyedstudent-max <iyedstudent@gmail.com>
2026-01-01 05:48:15 +0100

    Update loading.js

--- loading.js
+++ loading.js
@@ -1,30 +1,31 @@
-console.log("HELP");
 $(function(){
-  $("#git_photo").on({ mouseenter: function(){
-    $(this).attr('src','img/git_o.png');
-  },
-  mouseleave: function(){
-    $(this).attr('src','img/git.png');
-  }
+  $("#git_photo").on({
+    mouseenter: function(){
+      $(this).attr('src','git_o.png');
+    },
+    mouseleave: function(){
+      $(this).attr('src','git.png');
+    }
   });
 
 
-
-  $("#bash_photo").on({ mouseenter: function(){
-    $(this).attr('src','img/bash_o.png');
-  },
-  mouseleave: function(){
-    $(this).attr('src','img/bash.png');
-  }
+  $("#bash_photo").on({
+    mouseenter: function(){
+      $(this).attr('src','bash_o.png');
+    },
+    mouseleave: function(){
+      $(this).attr('src','bash.png');
+    }
   });
   
 
 
-  $("#github_photo").on({ mouseenter: function(){
-    $(this).attr('src','img/github_o.png');
-  },
-  mouseleave: function(){
-    $(this).attr('src','img/github.png');
-  }
+  $("#github_photo").on({
+    mouseenter: function(){
+      $(this).attr('src','github_o.png');
+    },
+    mouseleave: function(){
+      $(this).attr('src','github.png');
+    }
   });
 });
